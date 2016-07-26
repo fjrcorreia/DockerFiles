a9a8061a30ebe4d8074cf915cdcc0c19c4231780
Francisco Correia <francisco.correia.pt@gmail.com>
2016-07-26 23:28:08 +0100

    minor update

--- src/wildfly/scripts/wildfly-launch.sh
+++ src/wildfly/scripts/wildfly-launch.sh
@@ -5,8 +5,8 @@ JBOSS_OPTS=${JBOSS_OPTS:-}
 IPADDR=${IPADDR:-$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | sort | head -n 1)}
 
 
-JBOSS_JAVA_OPTS="-Djboss.bind.address=$IPADDR -Djboss.bind.address.management=$IPADDR"
-JBOSS_JAVA_OPTS="${JBOSS_JAVA_OPTS} -Djboss.node.name=$HOSTNAME"
+JBOSS_JAVA_OPTS="-Djboss.bind.address=${IPADDR} -Djboss.bind.address.management=${IPADDR}"
+JBOSS_JAVA_OPTS="${JBOSS_JAVA_OPTS} -Djboss.node.name=${HOSTNAME}"
 
 
 
@@ -19,10 +19,6 @@ JBOSS_PASSOWRD=${JBOSS_PASSOWRD:-Abc!12345}
 JBOSS_INIT_DOMAIN=.init-domain
 
 
-
-
-## JBoss Wildfly as a Service config
-JBOSS_PIDFILE=/tmp/jboss.pid
 
 
 start_jboss() {
@@ -42,26 +38,26 @@ stop_boss() {
   echo "I [WILDFLY-INIT]: Stopping JBOSS on background!"
   count=0;
 
-  echo "PIDF: $JBOSS_PIDFILE"
-  if [ -f $JBOSS_PIDFILE ]; then
-    kpid=$(cat $JBOSS_PIDFILE)
+  if [ -f ${JBOSS_PIDFILE} ]; then
+    kpid=$(cat ${JBOSS_PIDFILE})
     ## 30 seconds wait
     let kwait=30
 
-    echo "PID: $kpid"
     # Try issuing SIGTERM
-    kill -15 $kpid
-    until [ `ps --pid $kpid 2> /dev/null | grep -c $kpid 2> /dev/null` -eq '0' ] || [ $count -gt $kwait ]
+    kill -15 ${kpid}
+    until [ `ps --pid ${kpid} 2> /dev/null | grep -c ${kpid} 2> /dev/null` -eq '0' ] || [ ${count} -gt ${kwait} ]
     do
       sleep 1
-      let count=$count+1;
+      let count=${count}+1;
     done
 
-    if [ $count -gt $kwait ]; then
-      kill -9 $kpid
+    if [ ${count} -gt ${kwait} ]; then
+      kill -9 ${kpid}
     fi
   fi
-  rm -f $JBOSS_PIDFILE
+
+  sleep 2
+  unset JBOSS_PIDFILE
   echo "I [WILDFLY-INIT]: Stopped!"
 }
 
@@ -83,7 +79,7 @@ assert_result() {
 #   None
 # Returns:
 #   success - if there is any scripts
-#   fail - if no scripts were found
+#   fail    - if no scripts were found
 #######################################
 has_init_scripts(){
   if [ $(ls /docker-init.d/*.sh 2&> /dev/null | wc -l ) -gt 0 ]  \
@@ -103,16 +99,16 @@ has_init_scripts(){
 ###############################################################################
 # Perform container initializatoin
 #
-#  - Creates an adminstration user, usefull for remote depoloy
+#  - Creates an adminstration user, usefull for remote deploy
 #  - Verifies if there are any init action to be performed (/docker-init.d)
 ###############################################################################
-if [ ! -e ${JBOSS_HOME}/$JBOSS_INIT_DOMAIN ]
+if [ ! -e ${JBOSS_HOME}/${JBOSS_INIT_DOMAIN} ]
 then
     JBOSS_INIT_ACTIONS=""
 
     JBOSS_CLI_CMD="${JBOSS_HOME}/bin/jboss-cli.sh --connect "
 
-    if [ ! -z $JBOSS_PASSOWRD ]
+    if [ ! -z ${JBOSS_PASSOWRD} ]
     then
         echo "I [WILDFLY-INIT]: Adding Administrator User: {${JBOSS_USER}}"
         # set admin user to jboss
@@ -123,8 +119,8 @@ then
            exit 1
         fi
         JBOSS_INIT_ACTIONS="${JBOSS_INIT_ACTIONS}SET ADMINISTRATOR: ${JBOSS_USER}\n"
-        JBOSS_CLI_CMD="$JBOSS_CLI_CMD"
     fi
+
 
 
     ## Start the server only if necessary
@@ -137,13 +133,13 @@ then
     		        *.sh)
     		            echo "I [WILDFLY-INIT]: Running Script: $i";
     		            JBOSS_INIT_ACTIONS="${JBOSS_INIT_ACTIONS} Exec Script [$i]:";
-    		            . "$i" && RES=$? && assert_result $RES
+    		            . "$i" && RES=$? && assert_result ${RES}
     		            JBOSS_INIT_ACTIONS="${JBOSS_INIT_ACTIONS} OK\n";
     		        ;;
         				*.cli)
         				    echo "I [WILDFLY-INIT]: Running Commands: $i";
         		            JBOSS_INIT_ACTIONS="${JBOSS_INIT_ACTIONS} Exec Commands [$i]:";
-        				    ${JBOSS_CLI_CMD} < $i && RES=$? && assert_result $RES
+        				    ${JBOSS_CLI_CMD} < $i && RES=$? && assert_result ${RES}
         		            JBOSS_INIT_ACTIONS="${JBOSS_INIT_ACTIONS} OK\n";
         				;;
         				*)
